2b88126dd01b47147cbec7ca2bea665336e97553
DustinEMiller <dustinmiller032@gmail.com>
2019-02-27 20:49:10 -0600

    adding context clue system

---
+++ Assets/Scripts/Player Scripts/ContextClue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextClue : MonoBehaviour {
+
+    public GameObject contextClue;
+
+    public void Enable() {
+        contextClue.SetActive(true);
+    }
+
+    public void Disable() {
+        contextClue.SetActive(false);
+    }
+}
--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Sign : MonoBehaviour {
 
+    public Signal contextOn;
+    public Signal contextOff;
     public GameObject dialogBox;
     public Text dialogText;
     public string dialog;
@@ -30,12 +32,14 @@ public class Sign : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
+            contextOn.Raise();
             playerInRange = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Player")) {
+            contextOff.Raise();
             playerInRange = true;
             dialogBox.SetActive(false);
         }
