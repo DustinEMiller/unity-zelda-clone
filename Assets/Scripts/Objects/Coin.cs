845e8f754fec46aa74f0e41221e0afd748df1467
DustinEMiller <dustinmiller032@gmail.com>
2019-03-13 20:29:31 -0500

    coin item

---
+++ Assets/Scripts/Objects/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : PowerUp {
+
+    public Inventory playerInventory;
+
+    void Start() {
+        powerupSignal.Raise();
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other) {
+  
+        if (other.gameObject.GetComponent<PlayerMovement>() && !other.isTrigger) {
+            playerInventory.coins++;
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+        }
+
+    }
+}
